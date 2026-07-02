6c6dba7da07a7bff94d2bec9868210590b8e2b86
Raul Catalan <raul.catalan@belvo.com>
2026-07-02 13:23:51 +0200

    [Added][XP-10113]: Enforce const arrow functions over function declarations.
    Aligns the shared ESLint config with Belvo FE code style by requiring const-assigned arrow functions instead of function declarations and expressions.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,14 @@
 import belvoConfig from './index.js';
 
-export default belvoConfig;
+export default [
+  ...belvoConfig,
+  {
+    name: 'app/eslint-custom-rules-exemption',
+    files: ['eslint-custom-rules/**/*.js'],
+    rules: {
+      'func-style': 'off',
+      'prefer-arrow-callback': 'off',
+      'no-restricted-syntax': 'off'
+    }
+  }
+];
